5d56a9c851a7f9d292e2b5cccbbc3c4656f032ca
alisamirali <alisamir.2020@azhar.edu.eg>
2022-05-12 18:10:47 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ali-Samir-Portfolio
+
+## It is constantly updated.
+
+![White Green Modern Minimalist Financial Consultant Facebook Cover](https://user-images.githubusercontent.com/62913154/167028243-180ba5eb-1392-4815-9460-55fc27ec488d.png)
---
+++ js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  $("#menu").click(function () {
+    $(this).toggleClass("fa-times");
+    $("header").toggleClass("toggle");
+  });
+
+  $(window).on("scroll load", function () {
+    $("#menu").removeClass("fa-times");
+    $("header").removeClass("toggle");
+  });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 700) {
+      $("#to-top").fadeIn();
+    } else {
+      $("#to-top").fadeOut();
+    }
+  });
+
+  $("#to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1000);
+  });
+});
+
+// Typing Animation
+var options = {
+  strings: [
+    "Full Stack Developer",
+    "Front-End Dev",
+    "Back-End Dev",
+    "Web Developer",
+    "Software Dev",
+  ],
+  typeSpeed: 30,
+  backSpeed: 30,
+  loop: true,
+  loopCount: Infinity,
+  cursorChar: ".",
+  backDelay: 3000,
+  startDelay: 0,
+  smartBackspace: true,
+};
+
+var typed = new Typed(".element", options);
