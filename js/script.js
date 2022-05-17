052e4a8a415323d8adcbc17dbb60827c7a1b7cbe
Ali Samir <alisamir.2020@azhar.edu.eg>
2022-05-17 20:21:42 +0200

    New Projects

--- js/script.js
+++ js/script.js
@@ -1,43 +1,43 @@
-$(document).ready(function () {
-  $("#menu").click(function () {
-    $(this).toggleClass("fa-times");
-    $("header").toggleClass("toggle");
-  });
+$(document).ready(function() {
+    $("#menu").click(function() {
+        $(this).toggleClass("fa-times");
+        $("header").toggleClass("toggle");
+    });
 
-  $(window).on("scroll load", function () {
-    $("#menu").removeClass("fa-times");
-    $("header").removeClass("toggle");
-  });
-  $(window).scroll(function () {
-    if ($(this).scrollTop() >= 700) {
-      $("#to-top").fadeIn();
-    } else {
-      $("#to-top").fadeOut();
-    }
-  });
+    $(window).on("scroll load", function() {
+        $("#menu").removeClass("fa-times");
+        $("header").removeClass("toggle");
+    });
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 700) {
+            $("#to-top").fadeIn();
+        } else {
+            $("#to-top").fadeOut();
+        }
+    });
 
-  $("#to-top").click(function () {
-    $("html, body").animate({ scrollTop: 0 }, 1000);
-  });
+    $("#to-top").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, 1000);
+    });
 });
 
 // Typing Animation
 var options = {
-  strings: [
-    "Full Stack Developer",
-    "Front-End Dev",
-    "Back-End Dev",
-    "Web Developer",
-    "Software Dev",
-  ],
-  typeSpeed: 30,
-  backSpeed: 30,
-  loop: true,
-  loopCount: Infinity,
-  cursorChar: ".",
-  backDelay: 3000,
-  startDelay: 0,
-  smartBackspace: true,
+    strings: [
+        "Full Stack Developer",
+        "Front-End Dev",
+        "Back-End Dev",
+        "Web Developer",
+        "Software Dev",
+    ],
+    typeSpeed: 30,
+    backSpeed: 30,
+    loop: true,
+    loopCount: Infinity,
+    cursorChar: ".",
+    backDelay: 3000,
+    startDelay: 800,
+    smartBackspace: true,
 };
 
 var typed = new Typed(".element", options);
